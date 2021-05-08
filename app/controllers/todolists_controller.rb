4bced9232db5dd663aa70f0eed2defaf5d357f8a
yutomorita416 <yutomorita416@gmail.com>
2021-05-08 03:53:18 +0000

    finish chapter7

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -31,7 +31,7 @@ class TodolistsController < ApplicationController
   
   private
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
   
 end
